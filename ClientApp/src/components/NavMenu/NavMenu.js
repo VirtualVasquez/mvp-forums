42b629ab54c4b3ad34bd9b23f189f6975dd931e0
CITADEL\mvasq <mvasq95@hotmail.com>
2023-07-03 23:12:40 -0400

    converted NavMenu from a class-based component into a functional compnenent that uses React hooks

--- ClientApp/src/components/NavMenu/NavMenu.js
+++ ClientApp/src/components/NavMenu/NavMenu.js
@@ -1,86 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.scss';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+export const NavMenu = ({ tempTestAuth, setShowLoginForm }) => {
+  const [collapsed, setCollapsed] = useState(true);
 
-  constructor (props) {
-    super(props);
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.toggleLogin = this.toggleLogin.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  toggleLogin = (navItem) => {
-    if(navItem === "login"){
-      this.props.setShowLoginForm(true);
+  const toggleLogin = (navItem) => {
+    if (navItem === 'login') {
+      setShowLoginForm(true);
       console.log(true);
-    }    
-    if(navItem === "signup"){
-      this.props.setShowLoginForm(false);
+    }
+    if (navItem === 'signup') {
+      setShowLoginForm(false);
       console.log(false);
     }
-  }
+  };
 
-  render () {
-    const { tempTestAuth } = this.props;
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/home">mvp_forums</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            {tempTestAuth ? 
-              (
-                <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-                  <ul className="navbar-nav flex-grow">
-                    <NavItem>
-                      <NavLink tag={Link} className="text-dark" to="/home">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink tag={Link} className="text-dark" to="/fetch-data" disabled><strong>LogOut</strong></NavLink>
-                    </NavItem>
-                  </ul>
-                </Collapse>
-              ) : 
-              (
-                <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-                <ul className="navbar-nav flex-grow">
-                  <NavItem>
-                    <NavLink 
-                      className="text-dark"
-                      onClick={() => this.toggleLogin('login')}
-                    >
-                      Login
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink 
-                      className="text-dark" 
-                      to="/fetch-data"
-                      onClick={() => this.toggleLogin('signup')}
-                    >Sign Up
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Container>
+          <NavbarBrand tag={Link} to="/home">mvp_forums</NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          {tempTestAuth ? (
+            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+              <ul className="navbar-nav flex-grow">
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/home">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data" disabled><strong>LogOut</strong></NavLink>
+                </NavItem>
+              </ul>
+            </Collapse>
+          ) : (
+            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+              <ul className="navbar-nav flex-grow">
+                <NavItem>
+                  <NavLink
+                    className="text-dark"
+                    onClick={() => toggleLogin('login')}
+                  >
+                    Login
                   </NavLink>
-                  </NavItem>
-                </ul>
-                </Collapse>
-              )
-            }
-
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
-}
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className="text-dark"
+                    to="/fetch-data"
+                    onClick={() => toggleLogin('signup')}
+                  >
+                    Sign Up
+                  </NavLink>
+                </NavItem>
+              </ul>
+            </Collapse>
+          )}
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
