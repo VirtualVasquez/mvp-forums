cc6ad210f042f8ce689909b5c9f745e4d69b2dfe
CITADEL\mvasq <mvasq95@hotmail.com>
2023-07-06 22:40:37 -0400

    added button to go to reply form. Still need to add functionality to button and create reply form

--- ClientApp/src/pages/ThreadPage/ThreadPage.js
+++ ClientApp/src/pages/ThreadPage/ThreadPage.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ThreadHeader from '../../components/ThreadHeader/ThreadHeader';
 import Pagination from '../../components/Pagination/Pagination';
 import Thread from '../../components/Thread/Thread';
+import './ThreadPage.scss'
+
 
 function ThreadPage () {
 
     return (
-      <div>
+      <div className="thread-page">
         <ThreadHeader />
+        <div className="button-container">
+          <button className="to-reply-form">
+            <strong>REPLY TO THIS TOPIC</strong>
+          </button>
+        </div>
+
         <Pagination />
         <Thread />
         <Pagination />
