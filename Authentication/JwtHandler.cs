40317d9359dd99d4923b3bb8d8613e0525fcc170
CITADEL\mvasq <mvasq95@hotmail.com>
2023-07-09 15:48:17 -0400

    constructing files needed for server/database

---
+++ Authentication/JwtHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_new_app.Authentication
+{
+    public class JwtHandler
+    {
+    }
+}
