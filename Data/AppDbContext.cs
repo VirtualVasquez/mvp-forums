75170fb66b8e439b162d7a2417e68768ba3d1f82
CITADEL\mvasq <mvasq95@hotmail.com>
2023-07-24 15:35:04 -0400

    constructed model file for Forum table. Updated AppDBContext to include forums.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,16 +1,16 @@
 ﻿using User.Data.Models;
+using Forum.Data.Models;
 
 using Microsoft.EntityFrameworkCore;
-
-namespace User.Data.Models;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+    public DbSet<User.Data.Models.User> Users { get; set; }
+    public DbSet<User.Data.Models.RefreshToken> RefreshTokens { get; set; }
+    public DbSet<Forum.Data.Models.Forum> Forums { get; }
 
-    public DbSet<User> Users { get; set; }
-    public DbSet<RefreshToken> RefreshTokens { get; set; }
 
 }
---
+++ Data/Models/Forum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Forum.Data.Models
+{
+    [Table("forums")]
+    public class Forum
+    {
+        [Column("id")]
+        public int Id { get; }
+
+        [Column("title")]
+        public string Title { get; }
+
+        [Column("description")]
+        public string Description { get; }
+
+        [Column("date_created")]
+        public DateTime DateCreated { get; }
+
+    }
+}
