9736341615fe1b35241f6bec2a70def459bbf9a3
CITADEL\mvasq <mvasq95@hotmail.com>
2023-08-07 17:41:53 -0400

    Added socials in footer. updated styling to better match navbar

--- ClientApp/src/components/Footer/Footer.js
+++ ClientApp/src/components/Footer/Footer.js
@@ -6,12 +6,12 @@ function Footer () {
     return (
         <footer>
             <ul className="social-links">
-                <li><a>Github</a></li>
-                <li><a>LinkedIn</a></li>
-                <li><a>Website</a></li>
+                <li><a href="https://github.com/VirtualVasquez">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/melvin-vasquez/">LinkedIn</a></li>
+                <li><a href="https://melvinvasquez.com/">Website</a></li>
             </ul>
             <div>
-                <p>MVP Forums</p>
+                <p>mvp_forums</p>
                 <p>Created by Melvin Vasquez</p>    
             </div>
         </footer>
