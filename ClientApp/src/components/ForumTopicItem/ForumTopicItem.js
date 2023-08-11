a592066b6372de4bb4c5dca4cb15e0cc6a6419c9
CITADEL\mvasq <mvasq95@hotmail.com>
2023-08-11 04:42:39 -0400

    refactored async functions into callbacks, fixed useEffects, no longer running endlessly

--- ClientApp/src/components/ForumTopicItem/ForumTopicItem.js
+++ ClientApp/src/components/ForumTopicItem/ForumTopicItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from "axios";
 import './ForumTopicItem.scss';
 
@@ -10,46 +10,48 @@ function ForumTopicItem ({topicId, userId, title, dateCreated, slug, currentPage
     const [authorOfMostRecent, setAuthorOfMostRecent] = useState(null);
     const [viewTotal, setViewTotal] = useState(null);
 
-    async function getTopicCreatorById(id){
-        try{
-            await axios.get(`/api/User/NameById/${id}`).then(response => {
+    const getTopicCreatorById = useCallback(async () => {
+        try {
+            await axios.get(`/api/User/NameById/${userId}`).then(response => {
                 setTopicCreator(response.data);
             })
-        } catch (error){
+        } catch (error) {
             console.error(error);
         }
-    };
+    }, [userId]);
 
-    async function getAuthorofLastPostById(id){
-        try{
-            await axios.get(`/api/User/NameById/${id}`).then(response => {
-                setAuthorOfMostRecent(response.data);
-            })
-        } catch (error){
-            console.error(error);
+    const getAuthorofLastPostById = useCallback(async () => {
+        if (mostRecentPost) {
+            try {
+                await axios.get(`/api/User/NameById/${mostRecentPost.userId}`).then(response => {
+                    setAuthorOfMostRecent(response.data);
+                })
+            } catch (error) {
+                console.error(error);
+            }
         }
-    };
+    }, [mostRecentPost]);
     
-    async function getTotalPostsByTopicId(id){
+    const getTotalPostsByTopicId = useCallback(async () => {
         try {
-            const response = await axios.get(`/api/Post/AllPostsByTopicId/${id}?page=${currentPage}&pageSize=9`);
+            const response = await axios.get(`/api/Post/AllPostsByTopicId/${topicId}?page=${currentPage}&pageSize=9`);
             const { totalPosts, lastPost } = response.data;
             setTotalReplies(formatNumber(totalPosts));
             setMostRecentPost(lastPost);
         } catch (error) {
             console.error(error);
         }
-    }
+    }, [topicId, currentPage])
 
-    async function getViewsByTopicId(id){
-        try{
-            await axios.get(`/api/View/TotalViews/${id}`).then(response => {
+    const getViewsByTopicId = useCallback(async () => {
+        try {
+            await axios.get(`/api/View/TotalViews/${topicId}`).then(response => {
                 setViewTotal(formatNumber(response.data));
             })
-        } catch (error){
+        } catch (error) {
             console.error(error);
         }
-    }
+    }, [topicId])
         
     function longFormatTimestamp(timestamp) {
         const now = new Date();
@@ -126,18 +128,20 @@ function ForumTopicItem ({topicId, userId, title, dateCreated, slug, currentPage
     }
             
     useEffect(() => {
-        getTopicCreatorById(userId);
-        getTotalPostsByTopicId(topicId);
-        getViewsByTopicId(topicId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
+        async function fetchDataOne() {
+            getTopicCreatorById();
+            getTotalPostsByTopicId();
+            getViewsByTopicId();
+        }
+        fetchDataOne();
+    }, [getTopicCreatorById, getTotalPostsByTopicId, getViewsByTopicId]);
 
     useEffect(() => {
-        if(mostRecentPost){
-            getAuthorofLastPostById(mostRecentPost.userId);
+        async function fetchDataTwo() {
+            getAuthorofLastPostById();
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+        fetchDataTwo();
+    }, [getAuthorofLastPostById])
 
     return (
         <li className="forumTopicItem box-shadow">
